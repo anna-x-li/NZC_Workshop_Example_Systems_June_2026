ed026b2678346ff455cc4d43d0d36b87cb62d054
HongxiLuo <hl8692@princeton.edu>
2026-06-24 14:52:04 +0800

    Add files via upload

--- NZC_Power Sector_12day_WIP/case/run.jl
+++ NZC_Power Sector_12day_WIP/case/run.jl
@@ -1,57 +1,6 @@
-import Pkg
-Pkg.activate("../MacroEnergy.jl")
-Pkg.add("Gurobi")
-
 using MacroEnergy
 using Gurobi
 using Logging
-using JuMP
-
-case = MacroEnergy.load_case(@__DIR__)
-optim = MacroEnergy.create_optimizer(Gurobi.Optimizer, nothing, ("Method" => 2, "Crossover" => 0, "BarConvTol" => 1e-3))
-
-model = MacroEnergy.generate_model(case, optim)
-
-MacroEnergy.optimize!(model)
-
-MacroEnergy.compute_conflict!(model)
-list_of_conflicting_constraints = MacroEnergy.ConstraintRef[];
-for (F, S) in MacroEnergy.list_of_constraint_types(model)
-    for con in MacroEnergy.JuMP.all_constraints(model, F, S)
-        if MacroEnergy.JuMP.get_attribute(con, MacroEnergy.MOI.ConstraintConflictStatus()) == MacroEnergy.MOI.IN_CONFLICT
-            push!(list_of_conflicting_constraints, con)
-        end
-    end
-end
-display(list_of_conflicting_constraints)
-
-# Save the list of conflicting constraints to a text file
-function clean_constraint_list(input_list::Vector{JuMP.ConstraintRef})
-    seen_patterns = Set{String}()
-    cleaned_list = String[] # We return strings for the text file
-
-    for constraint in input_list
-        line = string(constraint)
-        normalized = replace(line, r"\[\d+\]" => "[]")
-        if !(normalized in seen_patterns)
-            push!(seen_patterns, normalized)
-            push!(cleaned_list, line) 
-        end
-    end
-
-    return cleaned_list
-end
-
-result = clean_constraint_list(list_of_conflicting_constraints)
-
-open("conflicting_constraints.txt", "w") do io
-    for item in result
-        println(io, item)
-    end
-end
-
-
-# Run Case
 
 (system, model) = run_case(
     @__DIR__;
